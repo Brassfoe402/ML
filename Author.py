4e78e284d99e877d0c6100e3caa1eb91f6b7c6e1
Brassfoe402 <125663981+Brassfoe402@users.noreply.github.com>
2024-01-17 18:57:03 +0600

    Add files via upload

---
+++ Author.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Разработка Web-приложения для инференса моделей ML и анализа данных")
+st.write("Выполнил: Серебренников Арсений Алексеевич, МО-221")
+st.image("./data/photo.JPG", width=300)
